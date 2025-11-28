347a5721eebb86b0d1b02279608b0a31d1fd1a0f
Shawn Deng <dengguiheng@apitable.com>
2025-11-28 23:52:24 +0800

    chore: init

--- src/main/java/org/shawn/shaded/TbRestClient.java
+++ src/main/java/org/shawn/shaded/TbRestClient.java
@@ -2,10 +2,17 @@ package org.shawn.shaded;
 
 import org.thingsboard.rest.client.RestClient;
 
+/**
+ * Thingsboard Service.
+ */
 public class TbRestClient {
 
   private final RestClient restClient;
 
+  /**
+   * Constructor.
+   * @param restClient thingsboard REST client.
+   */
   public TbRestClient(RestClient restClient) {
     this.restClient = restClient;
   }
